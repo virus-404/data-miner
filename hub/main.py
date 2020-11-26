4a1050bbfa580af4b6407a8fdfda91e0c8a98278
AGAPITO GALLART <agb22@alumnes.udl.cat>
2020-11-26 20:52:56 +0100

    Creation of files SN

--- hub/main.py
+++ hub/main.py
@@ -1,6 +1,19 @@
+from multiprocessing import Process
+import os
 
-def main():
-    print("Server hub miner has started!")
+def info(title):
+    print(title)
+    print('module name:', __name__)
+    print('parent process:', os.getppid())
+    print('process id:', os.getpid())
 
-if __name__ == "__main__":
-    main()
+def f(name):
+    info('function f')
+    print('hello', name)
+
+if __name__ == '__main__':
+    info('main line')
+    p = Process(target=f, args=('bob',))
+    p.start()
+    p.join()
+
