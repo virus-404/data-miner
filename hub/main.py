d6cb94c9859deb6d05db16e6a6a04beb925d0cf6
AGAPITO GALLART <agb22@alumnes.udl.cat>
2020-12-02 13:28:17 +0100

    Hub process can call SN

--- hub/main.py
+++ hub/main.py
@@ -1,19 +1,23 @@
-from multiprocessing import Process
 import os
+import importlib
+from multiprocessing import Process
+
 
 def info(title):
     print(title)
     print('module name:', __name__)
     print('parent process:', os.getppid())
-    print('process id:', os.getpid())
+    print('process id 0:', os.getpid())
 
-def f(name):
-    info('function f')
-    print('hello', name)
+def exec_starter(name):
+    social_network = importlib.import_module(name+'.'+name)
+    starter = getattr(social_network, 'run')()
+    del starter
 
 if __name__ == '__main__':
     info('main line')
-    p = Process(target=f, args=('bob',))
+    p = Process(target=exec_starter, args=('google',))
     p.start()
     p.join()
-
+ 
+ #sort id text timestamp source symbols company_names url verified
